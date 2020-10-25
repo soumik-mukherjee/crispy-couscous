d9afc7db0ca2a4f566908149adba4ae366c6ceca
Soumik Mukherjee <60540758+soumik-mukherjee@users.noreply.github.com>
2020-10-25 19:33:53 +0530

    parameterizing service calls

--- src/services.js
+++ src/services.js
@@ -3,12 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const UserOnboardingJourneyService = {
   start: async () => {
-    const url = 'https://m8do6wfprk.execute-api.ap-south-1.amazonaws.com/dev/journey';
+    const url = `${process.env.JOURNEY_API_ENDPOINT}/journey`;
     const executionName = uuidv4();
 
     const executionParams = {
       input: "{}",
-      stateMachineArn: 'arn:aws:states:ap-south-1:167104743001:stateMachine:TeacherOnboardingJourneyStateMachine-HWuKR24Vgkqs',
+      stateMachineArn: `${process.env.JOURNEY_ARN}`,
       name: executionName,
     };
     const response = await fetch(url, {
